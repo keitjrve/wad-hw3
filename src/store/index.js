36ee281ca4ed85fc0482003cd3a757fd1cd0b945
markkuskoodi <markkus999@outlook.com>
2020-11-20 18:53:16 +0200

    added few things.

---
+++ src/store/index.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex);
+
+const state = {
+    posts: [],
+    users: [],
+    loggedIn: []
+}
+
+const getters = {}
+
+const actions = {
+   getPosts({commit}) {
+       axios.get('https://private-anon-bd07188f71-wad20postit.apiary-mock.com/posts').then(response => {
+           commit('SET_POSTS', response.data)
+       })
+   },
+   getUsers({commit}){
+       axios.get('https://private-anon-70efa6a1a7-wad20postit.apiary-mock.com/profiles').then(response => {
+           commit('SET_USERS', response.data)
+       })
+   },
+   getLoggedUser({commit}){
+       axios.get('https://private-anon-aee74b6ac8-wad20postit.apiary-mock.com/users/1').then(response => {
+           commit('SET_LOGGEDIN', response.data)
+       })
+   }
+}
+
+const mutations = {
+    SET_POSTS(state, posts){
+        state.posts = posts
+    },
+    SET_USERS(state, users){
+        state.users = users
+    },
+    SET_LOGGEDIN(state, loggedIn){
+        state.loggedIn = loggedIn
+    }
+}
+
+export default new Vuex.store({
+    state,
+    getters,
+    actions,
+    mutations
+})
