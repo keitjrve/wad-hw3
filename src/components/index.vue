eadfac36b8371b2370a51b2dfa94c53aa4cf88dd
markkuskoodi <markkus999@outlook.com>
2020-11-20 17:59:25 +0200

    kekw

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,14 +1,15 @@
 <template>
   <section class="main-container">
+    <h1>wassup</h1>
     <posts>
     </posts>
   </section>
 </template>
 
 <script>
-export default {
-name: "index"
-}
+    export default {
+        name: "index"
+    }
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import index from "./components/index";
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const routes = [
+  {path: "/index", component: index},
+];
+
+const router = new VueRouter({routes});
 
 new Vue({
+  router,
   render: h => h(App),
-  el: '#app',
-  data: {color: '#673AB7'}
 }).$mount('#app')
