32d335ee7bb8e1dea9730a089aa47bfc1c82856c
KeitJärve <keit.jarve@gmail.com>
2020-11-20 17:23:42 +0200

    login

--- src/App.vue
+++ src/App.vue
@@ -17,20 +17,5 @@ export default {
 </script>
 
 <style>
-/*#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}*/
 
-#app {
-  color: #40c4ff;
-}
-
-template {
-  color: blueviolet;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,4 +5,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  el: '#app',
+  data: {color: '#673AB7'}
 }).$mount('#app')
